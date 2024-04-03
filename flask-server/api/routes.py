2b82ea0a8ac85002aa47a348aad584ec68ee6c3f
Kelly Laptop <kellyr@kelly-latitudee6430>
2024-04-03 19:46:11 -0400

    added in zakhar

--- flask-server/api/routes.py
+++ flask-server/api/routes.py
@@ -3,18 +3,14 @@ from pymongo import MongoClient
 from api.data_structs import User
 from server import app
 
-#@app.route("/members")
-#
-#def members():
-#    return {"members": ["Member1", "Member2", "Member3"]}
-
 @app.route('/api/login', methods=["POST"])
 def login():
-    return jsonify({}), 200
+    return User().login()
 
-@app.route('/register/', methods=('GET', 'POST'))
-def register_user():
-    if request.method == 'GET':
-        return jsonify({}), 200
-    if request.method == 'POST':
-        return User().signup()
+@app.route('/api/signup', methods=["POST"])
+def signup():
+    return User().signup()
+
+@app.route('/api/delete', methods=["POST"])
+def signup():
+    return User().delete()
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from pymongo import MongoClient
-from api import routes
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017, username="hobbygator", password="yomomma")
@@ -8,13 +7,7 @@ hg_database = client.flask_db
 users_db = client.users
 forums_db = client.forums
 
-@app.route('/', methods=('GET', 'POST'))
-def index():
-    return "Hello"
-
-#@app.route('/signup/', methods=('POST'))
-#def signup():
-
+from api import routes
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
